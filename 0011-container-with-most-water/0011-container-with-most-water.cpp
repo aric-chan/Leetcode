9681f6f974ce10089ff8d91765c8e1c40425b12f
aric <31781145+aric-chan@users.noreply.github.com>
2024-11-17 17:36:11 -0800

    Time: 4 ms (21.75%), Space: 61.6 MB (75.91%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -3,7 +3,7 @@ public:
     int maxArea(vector<int>& height) {
         int left = 0;
         int right = height.size() - 1;
-        
+
         int maxarea = 0;
 
         while(left < right) {
@@ -15,8 +15,5 @@ public:
             }
         }
         return maxarea;
-
-
-
     }
 };
