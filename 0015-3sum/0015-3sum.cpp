cb67e09efb6bd4d63856b50b6b1ba6f98e78bdb0
aric <31781145+aric-chan@users.noreply.github.com>
2024-11-19 09:58:47 -0800

    Time: 55 ms (46.08%), Space: 27.4 MB (88.83%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,6 +7,7 @@ public:
         sort(first, last);
 
         for(int anchor = 0; anchor < nums.size()-2; anchor++) {
+            //skip current iteration if anchor is the same as last iteration
             if (anchor > 0 && nums[anchor] == nums[anchor - 1]) {
                 continue; 
             }
@@ -14,26 +15,26 @@ public:
                 int right = nums.size() - 1;
                 while(left < right) {
                     if(nums[left] + nums[right] + nums[anchor] < 0) {
-                        //move left
+                    //move left and skip duplicate | left < right to prevent "over moving"
                     int prevLeft = nums[left];
                     while (left < right && nums[left] == prevLeft) {
                         left++;
                     }
                         continue;
                     } else if (nums[left] + nums[right] + nums[anchor] > 0) {
-                        //move right
-                    int prevRight = nums[right];
-                    while (left < right && nums[right] == prevRight) {
-                        right--;
-                    }
+                        //move right and skip duplicate | left < right to prevent "over moving"
+                        int prevRight = nums[right];
+                        while (left < right && nums[right] == prevRight) {
+                            right--;
+                        }
                         continue;
                     } else {
                         result.push_back({nums[anchor], nums[left], nums[right]});
-                        int prev = nums[left];
-                    int prevLeft = nums[left];
-                    while (left < right && nums[left] == prevLeft) {
-                        left++;
-                    }
+                        //move left and skip duplicate | left < right to prevent "over moving"
+                        int prevLeft = nums[left];
+                        while (left < right && nums[left] == prevLeft) {
+                            left++;
+                        }
                         continue;
                     }
                 }
