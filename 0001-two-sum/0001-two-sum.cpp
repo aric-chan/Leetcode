42cafdc5e78f145cd43b4c2db5c97a832bbe6186
aric <31781145+aric-chan@users.noreply.github.com>
2024-12-17 01:55:55 -0800

    Time: 0 ms (100%), Space: 14.9 MB (18.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> targetdiff;
         vector<int> result;
-        unordered_map <int,int> map;
         for(int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            if(map.find(complement) != map.end()) {
-                //return corresponding index
-                result = {i, map[complement]};
-            } else {
-                //insert current value and its index as key,value pair to map
-                map[nums[i]] = i;
+            if(targetdiff.find(nums[i])!= targetdiff.end()){
+                result.push_back(targetdiff[nums[i]]);
+                result.push_back(i);
+                return result;
             }
+            int diff = target - nums[i];
+            targetdiff[diff] = i;
         }
         return result;
     }
