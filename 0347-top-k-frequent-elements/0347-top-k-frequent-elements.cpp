64718ad4f99afde8e88b08809816f6c2eb7bc6c1
aric <31781145+aric-chan@users.noreply.github.com>
2024-12-17 03:59:04 -0800

    Time: 18 ms (5.72%), Space: 18.2 MB (19.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freqMap;
+        multimap<int, int> order;
         vector<int> result;
-        unordered_map <int,int> umap;
-        priority_queue<pair<int, int>> myqueue;
+        for(auto i : nums) {
+            freqMap[i]++;
+        }
+        for(auto e : freqMap){
+            order.insert({e.second,e.first});
+            cout << e.second << e.first << endl;
+        }
 
-        for(int i = 0; i < nums.size(); i++) {
-            if(umap.find(nums[i]) != umap.end()) {
-                umap[nums[i]] ++;
+        for(auto i = order.rbegin(); i != order.rend(); i++) {
+            cout<<"order"<<i->first<<i->second<<endl;
+            if(k!=0) {
+                result.push_back(i->second);
             } else {
-                umap[nums[i]] = 1;
+                return result;
             }
+            k--;
         }
-
-        //insert all key-value pair in the form of value-key
-        for (auto& item : umap) {
-            myqueue.push({item.second, item.first});
-        }
-
-        // Extract the top k elements from queue
-        for (int i = 0; i < k; i++) {
-            // Add the number (second part of pair)
-            result.push_back(myqueue.top().second);  
-            myqueue.pop();
-        }
-
         return result;
     }
-
 };
