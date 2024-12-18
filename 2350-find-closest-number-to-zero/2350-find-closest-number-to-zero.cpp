e0fb5573f708933fd89e848394bcfe9fe3797adc
aric <31781145+aric-chan@users.noreply.github.com>
2024-12-17 16:09:12 -0800

    Time: 0 ms (100%), Space: 23.3 MB (17.1%) - LeetHub

--- 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.cpp
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int findClosestNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int last = nums.size() - 1;
-        int front = 0;
-        while(front != last) {
-            if(abs(nums[front]) > abs(nums[last])){
-                front++;
-            } else if(abs(nums[front]) < abs(nums[last])){
-                last--;
-            } else {
-                front++;
+        int closestsofar = nums[0];
+        for(auto n : nums) {
+            if(abs(closestsofar) > abs(n)) {
+                closestsofar = n;
             }
         }
-        return nums[front];
 
-
-
-        
+        for(auto n : nums){
+            if(closestsofar + n == 0 && n > closestsofar) {
+                closestsofar = n;
+            }
+        }
+        return closestsofar;
+        //O(nlogn) not good
+        // sort(nums.begin(),nums.end());
+        // int last = nums.size() - 1;
+        // int front = 0;
+        // while(front != last) {
+        //     if(abs(nums[front]) > abs(nums[last])){
+        //         front++;
+        //     } else if(abs(nums[front]) < abs(nums[last])){
+        //         last--;
+        //     } else {
+        //         front++;
+        //     }
+        // }
+        // return nums[front];
     }
 };
