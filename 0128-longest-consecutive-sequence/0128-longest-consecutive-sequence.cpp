a557b32cfb51fc925520ae6b3ddfcf6974e24a0f
aric <31781145+aric-chan@users.noreply.github.com>
2024-12-21 03:34:30 -0800

    Time: 1811 ms (5%), Space: 80.6 MB (5.4%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        map<int, int> arrayMap;
+        vector<int> array;
+        if(nums.size() == 0) return 0;
+        for(int n : nums){
+            arrayMap[n]++;
+        }
+
+        for(auto i = arrayMap.begin(); i != arrayMap.end(); i++){
+            array.push_back(i->first);
+        }
+        int count = 1;
+        int maxCount = 1;
+        for(int i = 1; i < array.size() ; i++){
+            if(array[i] - array[i - 1] == 1){
+                cout<<"count"<<count<<endl;
+                cout<<"array[i]"<<array[i]<<endl;
+                count++;
+            } else {
+                count = 1;
+            }
+            maxCount = max(maxCount, count);
+        }
+
+        return maxCount;
+    }
+};
