0f1d2012b95eed5642384826777bbed1799937cd
aric <31781145+aric-chan@users.noreply.github.com>
2024-12-28 14:47:21 -0800

    Time: 2 ms (4.52%), Space: 9.4 MB (6.92%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -4,16 +4,16 @@ public:
         string str = "";
         int count = 0;
         while(n != 0){
-            cout << "n : " << n << endl;
-            cout << "str *before : " << str << endl;
+            // cout << "n : " << n << endl;
+            // cout << "str *before : " << str << endl;
             str = to_string(n % 2) + str;
-            cout << "str *after : " << str << endl;
+            // cout << "str *after : " << str << endl;
             n = n / 2;
         }
         for(int i = 0; i < str.size();i++){
             if(str.at(i) == '1') count++;
         }
-        cout<<"count : "<<count<<endl;
+        // cout<<"count : "<<count<<endl;
 
         return count;
     }
